b24d0cb45662df66463c2e202c9234a5bd066781
danoseun <ayodejisomefun@yahoo.com>
2018-10-12 17:07:15 +0100

    [Feature #161145918]  Add script to open and close modal

---
+++ UI/js/cart.js
@@ -0,0 +1,32 @@
+const modal = document.querySelector(".modal");
+const productModal = document.querySelector(".productModal");
+
+const displayModal = () => {
+  modal.style.display = "block";
+};
+document.querySelector(".add-attendant").addEventListener("click", displayModal);
+
+const cancelModal = event => {
+  modal.style.display = "none";
+  productModal.style.display = "none";
+};
+document.querySelector(".cancel").addEventListener("click", cancelModal);
+document.querySelector(".close").addEventListener("click", cancelModal);
+
+window.onclick = event => {
+  if (event.target === modal || event.target === productModal) {
+    modal.style.display = "none";
+    productModal.style.display = "none";
+  }
+};
+
+const displayProduct = () => {
+    productModal.style.display = "block";
+  };
+
+const productName = document.querySelectorAll(".product-name");
+const product = document.querySelectorAll(".product");
+productName.forEach((name, index, nameArray) => {
+   nameArray[index].addEventListener('click', displayProduct);
+});
+
