87db0fe71db83e92bb806130ecbd8c9fcf62f295
danoseun <ayodejisomefun@yahoo.com>
2018-10-17 16:07:00 +0100

    [Chore #161286920]create dummy user details

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    /* eslint-disable no-console */
-    console.log(`Server is live on PORT: ${port}`);
+  /* eslint-disable no-console */
+  console.log(`Server is live on PORT: ${port}`);
 });
