a9b05803c4b9b3ca00b2ed8a7e60b1d496d854f7
danoseun <ayodejisomefun@yahoo.com>
2018-10-18 03:23:24 +0100

    [Chore #161288129] Add more user objects to user dummy model

--- server/dummyDb/user.js
+++ server/dummyDb/user.js
@@ -11,6 +11,18 @@ const users = [
     email: 'attendant1@sm.com',
     password: 'attendantpass1',
     role: 'attendant'
+  },
+  {
+    userId: 3,
+    email: 'attendant2@sm.com',
+    password: 'attendantpass2',
+    role: 'attendant'
+  },
+  {
+    userId: 4,
+    email: 'attendant3@sm.com',
+    password: 'attendantpass3',
+    role: 'attendant'
   }
 ];
 
