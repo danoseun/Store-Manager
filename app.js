fcf67ac4a54fefe82c8b57b88915928f1a79274f
danoseun <ayodejisomefun@yahoo.com>
2018-10-18 13:31:09 +0100

    [Feature #161304782] Change CRLF to LF

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import userRouter from './server/routes/user';
 
 // Create a top level instance of express
 const app = express();
@@ -10,8 +11,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Declare subroutes
+app.use('/api/v1', userRouter);
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Server is live on PORT: ${port}`);
 });
+
+export default app;
