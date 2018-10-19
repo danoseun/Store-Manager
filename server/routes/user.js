ec020ad8622b8a34a14546d0d221952edbc9aa13
danoseun <ayodejisomefun@yahoo.com>
2018-10-19 12:27:43 +0100

    [chore #161314884] Add authorization middleware to user routes file

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import UserController from '../controllers/user';
-import UserValidator from '../middlewares/user';
+import middlewares from '../middlewares';
 
 const { signUp, login } = UserController;
+const { UserValidator, VerifyRole } = middlewares;
 const { signUpValidator, loginValidator } = UserValidator;
+const { isAdmin, isAttendant } = VerifyRole;
 
 const userRouter = express.Router();
 
-userRouter.post('/auth/signUp', signUpValidator, signUp);
+userRouter.post('/auth/signUp', signUpValidator, isAdmin, signUp);
 userRouter.post('/auth/login', loginValidator, login);
 
 export default userRouter;
