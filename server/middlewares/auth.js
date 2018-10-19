996fc6887a9967fef154ebf68818416679d1212f
danoseun <ayodejisomefun@yahoo.com>
2018-10-19 12:28:30 +0100

    [chore #161314884] Create authorization middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,63 @@
+import users from '../dummyDb/user';
+
+/**
+ * Class representing User Authorization
+  @class VerifyRole
+ */
+class VerifyRole {
+/**
+ * isAdmin - Checks if user is admin
+ * @static
+ * @param {object} req - Request object
+ * @param {object} res - Response Object
+ * @param {object} next - Calls the next function/route handler
+ * @returns {object} JSON representing the failure message
+ */
+  static isAdmin(req, res, next) {
+    const { UserId } = req.body;
+    console.log('2', typeof UserId);
+    const foundUser = users.find(user => user.userId === UserId);
+    console.log('3', typeof foundUser);
+    if (foundUser) {
+      if (foundUser.role === 'admin') {
+        return next();
+      }
+      return res.status(401).json({
+        status: 'Fail',
+        message: 'You are not authorized to visit this page'
+      });
+    }
+    return res.status(422).json({
+      status: 'Fail',
+      message: 'Please login with the correct details if you have been signed up by the admin'
+    });
+  }
+
+  /**
+ * isAttendant - Checks if user is attendant
+ * @static
+ * @param {object} req - Request object
+ * @param {object} res - Response Object
+ * @param {object} next - Calls the next function/route handler
+ * @returns {object} JSON representing the failure message
+ */
+  static isAttendant(req, res, next) {
+    const { UserId } = req.body;
+    const foundUser = users.find(user => user.userId === UserId);
+    if (foundUser) {
+      if (foundUser.role === 'attendant') {
+        return next();
+      }
+      return res.status(401).json({
+        status: 'Fail',
+        message: 'You are not authorized to visit this page'
+      });
+    }
+    return res.status(422).json({
+      status: 'Fail',
+      message: 'Please login with the correct details if you have been signed up by the admin'
+    });
+  }
+}
+
+export default VerifyRole;
