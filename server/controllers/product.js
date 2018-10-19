bf33741afed008ec0ca41431fff38ebee6f2c87e
danoseun <ayodejisomefun@yahoo.com>
2018-10-19 20:26:07 +0100

    [Feature #161348313] Finish create product record functionality

---
+++ server/controllers/product.js
@@ -0,0 +1,38 @@
+import { products } from '../dummyDb';
+
+/**
+ * Class representing ProductController
+ * @class ProductController
+ */
+class ProductController {
+  /**
+     * Create new product record
+     * @static
+     * @param {object} req - The request object
+     * @param {object} res - The response object
+     * @return {object} JSON representing success message
+     * @memberof ProductController
+     */
+  static createProduct(req, res) {
+    const {
+      productname, description, category, imageurl, quantity, unitprice
+    } = req.body;
+    const id = products[products.length - 1].id + 1;
+    const newProduct = {
+      id,
+      productname,
+      description,
+      category,
+      imageurl,
+      quantity,
+      unitprice
+    };
+    products.push(newProduct);
+    return res.status(201).json({
+      newProduct,
+      message: 'Product record successfully created'
+    });
+  }
+}
+
+export default ProductController;
