6e66f613277cdc1711c1feeb7b5462b87ef01d12
danoseun <ayodejisomefun@yahoo.com>
2018-10-21 17:38:02 +0100

    [Chore #161364704] Add sales controller to index file

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
 import UserController from './user';
 import ProductController from './product';
+import SalesController from './sale';
 
 export default {
   UserController,
-  ProductController
+  ProductController,
+  SalesController
 };
